dc1b53403f90d4809a42d5bdf42b01b9074dad9d
BekjonCoder <k75154256@gmail.com>
2025-03-16 23:00:45 +0500

    dd

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict'
 const API='https://jsonplaceholder.typicode.com/users'
-// const getData=async()=>{
-//     return 0
-// }
-// const test=getData()
-// console.log(test);
+const getData=async(resource)=>{
+    const reponse=await fetch(resource)
+    const data=await reponse.json()
+    return data
+    
+    
+}
+    
+    
+    
+getData(API)
+.then((res)=>{
+    console.log(res);
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
 
 // console.log('First');
 // setTimeout(()=>{
