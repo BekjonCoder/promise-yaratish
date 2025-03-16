6f90d2c5d51e1e2b36549f66a473075a6c365b87
BekjonCoder <k75154256@gmail.com>
2025-03-16 22:09:19 +0500

    vv

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 'use strict'
 const API='https://jsonplaceholder.typicode.com/users'
-const getData=()=>{}
+// const getData=async()=>{
+//     return 0
+// }
+// const test=getData()
+// console.log(test);
+
